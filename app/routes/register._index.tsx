7056facfbd64e288cbf3eef02dce67bfd550af14
Franck <franckvienot7@gmail.com>
2024-10-17 20:38:25 +0200

    make a register page and modify bg

--- app/routes/register._index.tsx
+++ app/routes/register._index.tsx
@@ -20,6 +20,19 @@ const Login: React.FC = () => {
               required
             />
           </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+              Repeat Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              placeholder="Enter your email"
+              required
+            />
+          </div>
           <div className="mb-6">
             <label
               className="block text-gray-700 text-sm font-bold mb-2"
@@ -41,14 +54,8 @@ const Login: React.FC = () => {
               type="submit"
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
             >
-              Sign In
+              Register
             </button>
-            <Link
-              to="#"
-              className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-            >
-              Forgot Password?
-            </Link>
           </div>
         </form>
         <div className="mt-4 text-center">
